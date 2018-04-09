f6a92b8b0a4e495f68061e88be9b45c04de7f2d9
Balasubramanian M <mbasubram@gmail.com>
2018-04-09 22:41:46 +0530

    Added support for login

--- NormalOperation/connection.php
+++ NormalOperation/connection.php
@@ -3,7 +3,7 @@
 $message = '';
 $username = 'someuser';
 $password = 'hello123';
-$db = new mysqli('localhost', $username, $password, 'traffic');
+$db = new mysqli('localhost', $username, $password, 'trafficDatabase');
 
 if ($db->connect_error){
 	$message = $db->connect_error;
